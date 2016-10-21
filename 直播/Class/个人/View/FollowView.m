5923e5c21e0946c898e57a6c5f88519a71779e4e
tiantianlan <200832147@qq.com>
2016-10-21 11:47:37 +0800

    添加内容

--- 直播/Class/个人/View/FollowView.m
+++ 直播/Class/个人/View/FollowView.m
@@ -7,6 +7,15 @@
 //
 
 #import "FollowView.h"
+#import "Masonry.h"
+
+@interface FollowView()
+
+@property (nonatomic, strong) UIView *lineView;
+@property (nonatomic, strong) UIView *line2View;
+@property (nonatomic, strong) UIView *line3View;
+
+@end
 
 @implementation FollowView
 
@@ -15,9 +24,79 @@
     self = [super initWithFrame:frame];
     if (self)
     {
-        self.backgroundColor = [UIColor redColor];
+        [self addSubview:self.lineView];
+        [self addSubview:self.line2View];
+        [self addSubview:self.line3View];
+        [self addSubview:self.followLabel];
+        [self addLayout];
     }
     return self;
+}
+
+#pragma mark - 懒加载
+- (UILabel *)followLabel
+{
+    if (!_followLabel)
+    {
+        _followLabel = [[UILabel alloc] init];
+        _followLabel.numberOfLines = 0;
+        _followLabel.text = @"0 \n 关注";
+    }
+    return _followLabel;
+}
+- (UIView *)line2View
+{
+    if (!_line2View)
+    {
+        _line2View = [[UIView alloc] init];
+        _line2View.backgroundColor = [UIColor whiteColor];
+    }
+    return _line2View;
+}
+
+- (UIView *)line3View
+{
+    if (!_line3View)
+    {
+        _line3View = [[UIView alloc] init];
+        _line3View.backgroundColor = [UIColor whiteColor];
+    }
+    return _line3View;
+}
+
+- (UIView *)lineView
+{
+    if (!_lineView)
+    {
+        _lineView = [[UIView alloc] init];
+        _lineView.backgroundColor = [UIColor whiteColor];
+    }
+    return _lineView;
+}
+
+#pragma mark -Void
+- (void)addLayout
+{
+    [_lineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(self);
+        make.height.equalTo(@(0.5));
+        make.top.equalTo(self);
+    }];
+    
+    [_line2View mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(@(0.5));
+        make.top.equalTo(self).offset(20);
+        make.bottom.equalTo(self).offset(-20);
+        make.centerX.equalTo(self).multipliedBy(2.0 / 3);
+    }];
+    
+    [_line3View mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(@(0.5));
+        make.top.equalTo(self).offset(20);
+        make.bottom.equalTo(self).offset(-20);
+        make.centerX.equalTo(self).multipliedBy(4.0 / 3);
+    }];
+    
 }
 
 @end
